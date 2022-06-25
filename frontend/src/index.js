85ceb4f34d77ccf6bd5c6f31829a706cd3d9a426
Steve Griesmer <sgriesmer@comcast.net>
2022-06-25 15:13:40 -0400

    Remove Javascript from index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './App';
 
-const a = false;
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {a ? <App /> : <h1>Hello from JSX </h1>}
+    <App />
   </React.StrictMode>
 );
 
