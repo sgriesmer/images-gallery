20ee0409a5e75dbe5034ccbb2003efe97bb0e56d
Steve Griesmer <sgriesmer@comcast.net>
2022-06-29 21:43:53 -0400

    eslint and prettier setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,33 @@
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
-  const [word, setWord] = useState('');
+  const [word, setWord] = useState("");
 
   const handleSearchSubmit = (e) => {
-    e.preventDefault()
-    console.log(word)
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    e.preventDefault();
+    console.log(word);
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
       })
       .catch((err) => {
         console.log(err);
-      })
-    setWord('');
+      });
+    setWord("");
   };
 
   return (
     <div className="App">
-      <Header title="Images Gallery"/>
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
