2307197693a317d26342717d543ae2cb1e1da895
Steve Griesmer <sgriesmer@comcast.net>
2022-06-30 08:13:58 -0400

    deleted prettierrc.json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
   const [word, setWord] = useState('');
+  const [images, setImages] = useState([]);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -17,6 +18,7 @@ function App() {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+        setImages([...images]);
       })
       .catch((err) => {
         console.log(err);
