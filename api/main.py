5cf4cb4bf8e094e25aa64c05bb55901846c0ac06
Steve Griesmer <sgriesmer@comcast.net>
2022-07-11 00:00:30 -0400

    Eliminated indent on return data

--- api/main.py
+++ api/main.py
@@ -31,7 +31,7 @@ def new_image():
   params = {"query": word}
   response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
   data = response.json()
-    return data
+  return data
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
